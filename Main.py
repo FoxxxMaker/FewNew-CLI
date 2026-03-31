fe5bf025dda09e19c5d0e5c123f80e051b31a13e
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-03-31 13:28:52 -0300

    Melhoria no Menu_Principal e adição de um menu secundário

--- Main.py
+++ Main.py
@@ -1,25 +1,44 @@
 # esta parte é a interface gráfica do CLI.
 '''
 parte que receberá as importações futuras
-
+# import API
+# import Tradutor
+# import 
 '''
 
 class Menu():
+    def menu_simplificado():
+        # Definindo o menu (segunda parte)
+        print(" ")
+        
+
     def Menu_Principal():
-        # Definindo o menu (primeira parte)
+        # Definindo o menu (parte da primeira validação)
         print("Iniciando Programa....")
         print("Olá! Bem-vindo ao FewNew!")
 
         print(" ")
 
-        print("Pressione a tecla N para receber uma ideia nova!")
+        print("Pressione a tecla ENTER para receber uma ideia nova ou digite sair para sair")
 
-        answer = input()
+        answer = input("Pressione Enter ou sair....")
 
-        if answer.lower() == 'N':
-            # resposta dos arquivos que ainda serão editados
-            print("Ideia para base:")
-            print("Faça amigos!")
-        elif answer.lower() != 'n':
-            print("Por favor, digite apenas N.")
-            # Aqui virá outro menu resumido
+        if not answer:
+            print("Resposta") 
+            print(" ")
+
+            print("Deseja continuar?")
+            print("s/n")
+            answer = input("Digite s para sim ou n para não....")
+            
+            if answer.upper() == 'S':
+                Menu.menu_simplificado()
+            if answer.upper() == 'S':
+                print("Saindo....")
+
+        elif answer.lower() == "sair":
+            print("Saindo do Programa...")
+            
+    
+
+Menu.Menu_Principal()
