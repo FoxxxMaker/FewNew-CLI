6ba1a6f5334d2d9cce1bee4ea512410f298a5fff
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-03-31 13:38:07 -0300

    Começo da requisição da API

---
+++ Request.py
@@ -0,0 +1,10 @@
+# Este código fará a requisição para o API (Bored)
+
+import requests
+
+# mostra o caminho da url da API
+url = "https://bored-api.appbrewery.com/random"
+
+# Faz a requisição ao API e armazena a resposta em uma variável chamada 'resposta'.
+
+resposta = requests.get(url)
