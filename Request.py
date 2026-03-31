c7ccc1ee3b580d25c892a24fe2dede6ee71daa78
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-03-31 13:44:55 -0300

    Requests completa: Chama activity e guarda na variável atividade

--- Request.py
+++ Request.py
@@ -8,3 +8,11 @@ url = "https://bored-api.appbrewery.com/random"
 # Faz a requisição ao API e armazena a resposta em uma variável chamada 'resposta'.
 
 resposta = requests.get(url)
+try:
+    dados = resposta.json()
+
+    Atividade = dados["activity"]
+    print(Atividade)
+except Exception as e:
+    print("Erro na requisição", e)
+
