e0ce1e893a0aaaa9e347e1241ec3bbc6c8a9a9da
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-04-07 13:36:06 -0300

    Primeiro Label (inicio do Front-End)

--- FrontEnd/Front.py
+++ FrontEnd/Front.py
@@ -1,1 +1,12 @@
-import tkinter as Tk
+import tkinter as tk
+
+class app:
+    # primeira parte do front end:
+    root = tk.Tk()
+    root.title("FewNew")
+    root.geometry("400x400")
+
+    lbl1 = tk.Label(root, text="Clique no botão abaixo e receba uma nova ideia: ")
+    lbl1.pack(pady=20)
+
+    
