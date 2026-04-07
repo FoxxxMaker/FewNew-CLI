980ebb76d9ed6f47117c27beb6c819cd0d63afdf
Levi Soares <levisoaresp.alho.8c.m@gmail.com>
2026-04-07 13:49:39 -0300

    Função do Label Dinâmico

--- FrontEnd/Front.py
+++ FrontEnd/Front.py
@@ -1,4 +1,12 @@
 import tkinter as tk
+import Tradutor
+import Request as RQ
+
+def label_dinamico():
+    # Atualização de Label dinâmico
+    Atividade = RQ.buscar_atividade()
+    Tradu = Tradutor.Traduzir_texto(Atividade)
+    lblD.config(Tradu)
 
 class app:
     # primeira parte do front end:
@@ -6,7 +14,12 @@ class app:
     root.title("FewNew")
     root.geometry("400x400")
 
+    #Label de chamada
     lbl1 = tk.Label(root, text="Clique no botão abaixo e receba uma nova ideia: ")
     lbl1.pack(pady=20)
+
+    #Label Dinâmico
+    lblD = tk.Label(root,text=" ")
+    lblD.pack(pady=23)
 
     
